093f096a297fcdac761f1b2f96c7cde418cf6103
Srijan Choudhary <srijan4@gmail.com>
2018-12-27 00:52:38 +0530

    Updates for todoist script
    * Use env variable for token
    * Show task count
    * Add refresh button

--- todoist.1m.py
+++ todoist.1m.py
@@ -3,13 +3,14 @@
 
 # <bitbar.title>Todoist Today</bitbar.title>
 # <bitbar.version>v1.0.0</bitbar.version>
-# <bitbar.author>K.Kobayashi</bitbar.author>
-# <bitbar.author.github>kobayashi</bitbar.author.github>
+# <bitbar.author>K.Kobayashi and Srijan Choudhary</bitbar.author>
+# <bitbar.author.github>srijan</bitbar.author.github>
 # <bitbar.desc>Today's task in your menu bar!</bitbar.desc>
 # <bitbar.dependencies>python</bitbar.dependencies>
 # <bitbar.image>http://i.imgur.com/f37VtAg.png</bitbar.image>
 
 import sys
+import os
 if sys.version_info[0] < 3:
     from urllib2 import urlopen, Request
     from urllib import urlencode
@@ -19,28 +20,38 @@ else:
 import json
 import datetime
 
-api_key = ''
-url = 'https://todoist.com/API/v7/sync'
-value = { 'token': api_key, 'resource_types': '["all"]', 'seq_no': 0 }
-data = urlencode(value).encode('utf-8')
+api_key = os.environ.get('TODOIST_API_KEY')
 
-d = datetime.datetime.today()
-today = str(d.day)+d.strftime(" %b")
-today_y = str(d.day)+d.strftime(" %b ")+str(d.year)
-comment = "Today's task: "
+if not api_key:
+    print("Tasks: ☠")
+    print("---")
+    print("Please set TODOIST_API_KEY environment variable")
 
-if len(api_key) == 0:
-    print("set api first")
 else:
+    url = 'https://todoist.com/API/v7/sync'
+    value = { 'token': api_key, 'resource_types': '["items"]', 'seq_no': 0 }
+    data = urlencode(value).encode('utf-8')
+
+    d = datetime.datetime.today()
+    today = str(d.day)+d.strftime(" %b")
+    today_y = str(d.day)+d.strftime(" %b ")+str(d.year)
+
     try:
         request = Request(url, data)
         r = urlopen(request)
         j = json.loads(r.read())
         items = j['items']
+        today_items = []
         for item in items:
             due = item['date_string'] # due date of a task
             if (due == today) or (due == today_y):
-                print((comment + item['content']).encode('utf-8'))
+                today_items.append(item)
+        print("Tasks: %d" % len(today_items))
+        print("---")
+        for item in today_items:
+            print("⬜ " + item['content'])
     finally:
         r.close()
 
+print("---")
+print("Refresh... | refresh=true")
